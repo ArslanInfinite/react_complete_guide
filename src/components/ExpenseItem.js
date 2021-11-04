8038e627eb5493e4fc4a3c434bedad29be85d678
Arslan <ali.arslan319@gmail.com>
2021-11-03 23:59:29 -0400

    styled EI.js component by adding classNames to divs which point to corresponding css code blocks in EI.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
     return (
-        <div>
-            <div>Date: Today</div>
+        <div className='expense-item'>
+            <div className='expense-item__description'>Date: Today</div>
             <h2>Car Insurance</h2>
-            <div>$294.67</div>
+            <div className='expense-item__price'>$294.67</div>
         </div>)
 }
 
