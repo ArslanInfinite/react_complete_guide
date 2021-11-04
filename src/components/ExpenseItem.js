71e5e28c63bcf21ad1b3dad2bcb60ce097697d9d
Arslan <ali.arslan319@gmail.com>
2021-11-03 23:51:10 -0400

    add some elements to EI.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,10 @@
-function ExpenseItem(){
-    return <h2>Expense Item</h2>
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>
+            <h2>Time</h2>
+            <div>Amount</div>
+        </div>)
 }
 
 export default ExpenseItem
