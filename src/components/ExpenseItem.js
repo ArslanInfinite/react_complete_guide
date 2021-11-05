790844264a5e9fc542f323825310c8963bdbed3a
Arslan <ali.arslan319@gmail.com>
2021-11-04 23:31:17 -0400

    enter dynamic values via variables instead of hardcoding

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>Date: Today</div>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
+            <div className='expense-item__description'>{expenseDate.toISOString()}</div>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>)
 }
 
