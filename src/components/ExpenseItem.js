12b369a720a9917e310ed9b14135ba2a1f81bf19
Arslan <ali.arslan319@gmail.com>
2021-11-05 22:11:34 -0400

    use object destructuring to readily access attributes of props and pass them as values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 import './ExpenseItem.css'
-function ExpenseItem(props) {
+function ExpenseItem({title, amount, date}) {
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>{props.date.toISOString()}</div>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
+            <div className='expense-item__description'>{date.toISOString()}</div>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>${amount}</div>
         </div>)
 }
 
