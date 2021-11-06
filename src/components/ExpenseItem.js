cee8faf653682064546a863ac77cf71a8732b1ae
Arslan <ali.arslan319@gmail.com>
2021-11-05 22:08:57 -0400

    pass props from App.js to ExpenseItem.js to replace hardcoded variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28)
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = 294.67
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>{expenseDate.toISOString()}</div>
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price'>${expenseAmount}</div>
+            <div className='expense-item__description'>{props.date.toISOString()}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>)
 }
 
