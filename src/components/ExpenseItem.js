cdf732fd717e60b9bb5912b8e6331937c7403f2b
Arslan <ali.arslan319@gmail.com>
2021-11-05 23:04:38 -0400

    change date data of month, day, and year to be readable using built-in functions to access information

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './ExpenseItem.css'
 function ExpenseItem({title, amount, date}) {
+    const month = date.toLocaleString('en-US', { month: 'long'})
+    const day = date.toLocaleString('en-US', { month: '2-digit'})
+    const year = date.getFullYear()
+
+
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'>{date.toISOString()}</div>
+            <div>{month} </div>
+            <div>{day}</div>
+            <div>{year}</div>
+            <div className='expense-item__description'></div>
             <h2>{title}</h2>
             <div className='expense-item__price'>${amount}</div>
         </div>)
