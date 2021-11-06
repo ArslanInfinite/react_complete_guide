81848d465a6631615cb0c335900591d2f9073900
Arslan <ali.arslan319@gmail.com>
2021-11-05 23:40:59 -0400

    accurately pass props as date destructured from ExpenseItems to ExpenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ function ExpenseItem({title, amount, date}) {
 
     return (
         <div className='expense-item'>
-            <ExpenseDate {date}/>
+            <ExpenseDate date={date}/>
             <div className='expense-item__description'></div>
             <h2>{title}</h2>
             <div className='expense-item__price'>${amount}</div>
