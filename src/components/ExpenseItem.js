4718930914354a472ba11e74388232148bfb8a08
Arslan <ali.arslan319@gmail.com>
2021-11-05 23:05:57 -0400

    fix error month to day on line 4

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css'
 function ExpenseItem({title, amount, date}) {
-    const month = date.toLocaleString('en-US', { month: 'long'})
-    const day = date.toLocaleString('en-US', { month: '2-digit'})
+    const month = date.toLocaleString('en-US', { month: 'long' })
+    const day = date.toLocaleString('en-US', { day: '2-digit' })
     const year = date.getFullYear()
 
 
     return (
         <div className='expense-item'>
-            <div>{month} </div>
+            <div>{month}</div>
             <div>{day}</div>
             <div>{year}</div>
             <div className='expense-item__description'></div>
