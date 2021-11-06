6c2fe72d2abfe0d270c0a63adbb17a57600e2dcd
Arslan <ali.arslan319@gmail.com>
2021-11-05 23:36:39 -0400

    move date information to newly formed ExpensesDate.js component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 function ExpenseItem({title, amount, date}) {
-    const month = date.toLocaleString('en-US', { month: 'long' })
-    const day = date.toLocaleString('en-US', { day: '2-digit' })
-    const year = date.getFullYear()
-
 
     return (
         <div className='expense-item'>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
+            <ExpenseDate {date}/>
             <div className='expense-item__description'></div>
             <h2>{title}</h2>
             <div className='expense-item__price'>${amount}</div>
-        </div>)
+        </div>
+    )
 }
 
 export default ExpenseItem
