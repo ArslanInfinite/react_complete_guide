f1b37d2147c492cb612e8e73a2bbd17c84197d31
Arslan <ali.arslan319@gmail.com>
2021-11-08 23:57:47 -0500

    import useState and give it title argument to make changes to elements with eventhandlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ title, amount, date }) => {
+    useState({ title })
     const clickHandler= () => {
         console.log('Clicked')
     }
