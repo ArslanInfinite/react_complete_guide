1bb88df7335b33d131e7ed98ac03b37a7f1eb87f
Arslan <ali.arslan319@gmail.com>
2021-11-08 22:50:33 -0500

    create button and add clickHandler to test out React event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ title, amount, date }) => {
+    const clickHandler= () => {
+        console.log('Clicked')
+    }
 
     return (
         <Card className='expense-item'>
@@ -10,6 +13,7 @@ const ExpenseItem = ({ title, amount, date }) => {
             <div className='expense-item__description'></div>
             <h2>{title}</h2>
             <div className='expense-item__price'>${amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
