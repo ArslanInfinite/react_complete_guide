0eb3d1808c857cd8718fb42cfea823cb71414fe0
Arslan <ali.arslan319@gmail.com>
2021-11-09 23:59:51 -0500

    fill out component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,12 @@
 const ExpenseForm = () => {
-    
+    return <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type='text'/> 
+            </div>
+        </div>
+    </form>
 }
 
 export default ExpenseForm
