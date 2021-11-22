bbd429adf1ab096dea4d749d30ca0ec80e81360a
Arslan <ali.arslan319@gmail.com>
2021-11-21 23:52:06 -0500

    create ternary operator to display message when no expenses are present for year, otherwise continue to display expenses based on year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,18 +19,20 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter 
-                selected={filteredYear} 
-                onChangeFilter={filterChangeHandler} 
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpenses.map(expense => 
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                )}
+                {filteredExpenses.length === 0 ?
+                    <p>No Expenses Found</p> :
+                    filteredExpenses.map(expense =>
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    )}
             </Card>
         </div>
     )
