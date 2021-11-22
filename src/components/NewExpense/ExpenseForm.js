84abe8a26c3dcb776ae68b6334cbc1bb0540b5a8
Arslan <ali.arslan319@gmail.com>
2021-11-21 21:17:50 -0500

    convert ExpenseItem.js into a stateless-component to present information onto the DOM

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -66,17 +66,17 @@ const ExpenseForm = (props) => {
                 <div className='new-expense__control'>
                     <label>Amount</label>
                     <input type='number' 
-                    value={enteredAmount} 
                     min='0.01' 
                     step='0.01' 
+                    value={enteredAmount} 
                     onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
                     <input type='date' 
-                    value={enteredDate} 
                     min='2019-01-01' 
                     max='2022-12-31' 
+                    value={enteredDate} 
                     onChange={dateChangeHandler} />
                 </div>
             </div>
