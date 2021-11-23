2a9043ed625fa8f74817737f3480f16f51b4a271
Arslan <ali.arslan319@gmail.com>
2021-11-22 23:55:50 -0500

    create ExpensesList.js to extract filtered list from Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,19 +16,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear
     })
 
-    let expensesContent = <p>No Expenses Found</p>
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense =>
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        )
-    }
-
     return (
         <div>
             <Card className='expenses'>
