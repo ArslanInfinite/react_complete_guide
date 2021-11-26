908c8962e84949ee3e0aafdcc228b6ba90cfd1cd
Arslan <ali.arslan319@gmail.com>
2021-11-25 22:20:14 -0500

    iterate through array of potential Charts to create data point props for the future ChartBar component to receive

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,9 +3,14 @@ import './Chart.css'
 
 const Chart = props => {
   return <div className='chart'>
-    
+    {props.dataPoints.map(dataPoint => 
+    <ChartBar 
+    key={dataPoint.label}
+    value={dataPoint.value}
+    maxValue={null}
+    label={dataPoint.label}
+    />)}
   </div>
 }
 
 export default Chart
-
