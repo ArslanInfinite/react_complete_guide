7c45643714bff6777d1a485f40c19b8980ef5c8e
Arslan <ali.arslan319@gmail.com>
2022-01-01 18:00:25 -0500

    .

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,22 +1,22 @@
-// import ExpenseItem from "./ExpenseItem"
-// import './ExpensesList'
+import ExpenseItem from "./ExpenseItem"
+import './ExpensesList'
 
-// const ExpensesList = (props) => {
-//   if (props.items.length === 0) {
-//     return <h2 className='expenses-list__fallback'>Found No Expenses.</h2>
-//   }
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found No Expenses.</h2>
+  }
   
-//   return (
-//   <ul className='expenses-list'>
-//       {props.items.map(expense => 
-//       <ExpenseItem 
-//         key={expense.id} 
-//         title={expense.title} 
-//         amount={expense.amount} 
-//         date={expense.date}/>
-//         )}
-//   </ul>
-//   )
-// }
+  return (
+  <ul className='expenses-list'>
+      {props.items.map(expense => 
+      <ExpenseItem 
+        key={expense.id} 
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}/>
+        )}
+  </ul>
+  )
+}
 
-// export default ExpensesList
+export default ExpensesList
